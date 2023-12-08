d1666898b29b52d0f708c655fa19c35cb79a472d
Nestor Castillo <nestorraulcgomez@gmail.com>
2023-12-08 00:04:03 -0300

    Bootstrap added + ngIf and ngFor in Hero and HeroList components added

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroNames: string[] = ['Spiderman', 'Iroman', 'Hulk', 'She Hulk', 'Thor'];
+  public deletedHero?: string;
 
+  public removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
